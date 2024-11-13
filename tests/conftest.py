b2d2a0518b42325f75c37866a9cd7515b0be0286
xia__mc <2052472631@qq.com>
2024-11-13 19:38:18 +0800

    Fix pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import sys
 import os
 
+print("Debug:", os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
